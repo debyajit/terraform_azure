8ebecb012241c4a9a49e1ce36aecb58bf84def66
Deb <39284636+debyajit@users.noreply.github.com>
2025-11-03 17:03:38 -0500

    Reformat outputs.tf for consistency

--- outputs.tf
+++ outputs.tf
@@ -8,5 +8,5 @@ output "container_names" {
 
 output "staging_folder_paths" {
   description = "List of folders created inside the staging container"
-  value = [for folder in var.staging_folders : folder]
+  value       = [for folder in var.staging_folders : folder]
 }
